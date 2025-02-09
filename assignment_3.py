6176dd249b9f27969c3a5794fc662333af70c994
= <=>
2025-02-09 13:21:06 -0500

    Initial commit

---
+++ assignment_3.py
@@ -0,0 +1,13 @@
+'''
+
+For this assignment please do the following:
+
+- Read in the student_grades.csv file
+
+- calculate the average grade for the class
+
+- for each student record calculate the difference between the student's grade and the average grade
+
+- write the output to a new file called student_grade_differences.csv
+
+'''
